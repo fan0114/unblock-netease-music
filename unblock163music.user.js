bdb30b8d405cae63d721e76c68b6ca65cba29f41
Zheng Fan <fan0114@gmail.com>
2015-10-05 12:51:46 -0500

    Create unblock163music.user.js

---
+++ unblock163music.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       unblock 163 music
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      http://music.163.com/*
+// @copyright  2012+, You
+// ==/UserScript=
+
+GAbroad = false; _iframe = document.getElementById('g_iframe');_iframe.contentWindow.GAbroad = false;
